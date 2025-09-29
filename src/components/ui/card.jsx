a40fbf728927afceb77bc5d5c9332295c71ac26f
mraaziqp <52211507+mraaziqp@users.noreply.github.com>
2025-09-29 16:02:54 +0200

    Add files via upload

---
+++ src/components/ui/card.jsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+export function Card({ className, ...props }) {
+  return (
+    <div
+      className={`rounded-2xl border bg-white shadow-md ${className}`}
+      {...props}
+    />
+  );
+}
+
+export function CardContent({ className, ...props }) {
+  return (
+    <div className={`p-4 ${className}`} {...props} />
+  );
+}
